83c0fd9712aec49ace1eceeb0bc496174a0c4ccf
Lake <contact@lakeys.net>
2018-08-31 19:47:32 -0400

    Implement requirement loading
    The add-on now loads any copy of CityRPG it can find as a requirement.

--- Server_CityRPGPlus/prefs.cs
+++ Server_CityRPGPlus/prefs.cs
@@ -1,1 +1,1 @@
-registerPreferenceAddon("Server_CityRPGPlus", "CityRPG Plus", "building_add");
+//registerPreferenceAddon("Server_CityRPGPlus", "CityRPG Plus", "building_add");
--- Server_CityRPGPlus/server.cs
+++ Server_CityRPGPlus/server.cs
@@ -1,10 +1,47 @@
 // This is a script that enhances the CityRPG game-mode.
 // Developed by Lake Y.
 
+if($CityRPGPlus::Init && !$CityRPGPlus::ForceReload)
+{
+	echo("CityRPG Plus already loaded! Aborting...");
+	return;
+}
+$CityRPGPlus::Init = 1;
+
 exec("./prefs.cs");
 
+// If CityRPG is not alreay enabled, try to enable the first one we can find.
+// May execute the wrong one if there are multiple copies installed.
+if(!isObject(CityRPGData))
+  forceRequiredAddOn("Gamemode_CityRPG");
+if(!isObject(CityRPGData))
+  forceRequiredAddOn("GameMode_CityRPG_X");
+if(!isObject(CityRPGData))
+  forceRequiredAddOn("Gamemode_TysCityRPG");
+if(!isObject(CityRPGData))
+  forceRequiredAddOn("Gamemode_JJStormsCityRPG");
+
+// If the game-mode still doesn't exist, abort with a catch-all message.
+if(!isObject(CityRPGData)) {
+  // Reset init to 0 in case a custom branch re-runs the add-on.
+  $CityRPGPlus::Init = 0;
+
+  error("Server_CityRPGPlus - Unable to load a compatible version of CityRPG. This add-on requires CityRPG in order to run."
+	NL "If you are writing a custom CityRPG branch, have your mod detect and execute CityRPG Plus (forceRequiredAddOn won't work) after the game-mode initializes. Avoid renaming the CityRPGData object.");
+	return;
+}
+
+// # Package # //
 package CityRPGPlus
 {
 };
 deactivatePackage(CityRPGPlus);
 activatePackage(CityRPGPlus);
+
+// # Debug Functions # //
+function CityRPGPlus_Reload() {
+	$CityRPGPlus::ForceReload = 1;
+	exec("Add-Ons/Server_CityRPGPlus/server.cs");
+}
+
+echo("############ CityRPG & CityRPG Plus initialized! ############");
